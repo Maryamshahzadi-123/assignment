fb18fcb4cbee209e663215aa49c59e4452e17c0f
Maryamshahzadi-123 <rajpootmaryam268@gmail.com>
2024-08-28 12:10:44 -0700

    init repo code

---
+++ python-learning/calculator.py
@@ -0,0 +1,27 @@
+print("Addition")
+print("subtraction")
+print("multiplication")
+print("division")
+
+num1=int(input("enter your first num: "))
+num2=int(input("enter your second num: " ))
+opr=input("enter the opr :")
+
+if opr=="+":
+   print(num1+num2)
+elif opr=="-":
+   print(num1-num2)
+elif opr=="*":
+   print(num1*num2)
+elif opr=="/":
+   print(num1/num2)
+else :
+   print("invalid opr")
+
+
+         
+     
+
+
+
+
---
+++ python-learning/variable.py
@@ -0,0 +1,106 @@
+# 1.
+message = "Hello! I am good girl"
+print(message)
+
+# 2. 
+message = "Hello! World"
+print(message)
+message="update message "
+print(message)
+
+#3
+Person_name ="Maham"
+print(f'Hello {Person_name}, would you like to learn some Python today?')
+
+#4
+person_name ="Maham Fayyaz"
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
+
+#5
+quote=  "Start writing, no matter what. The water does not flow until the faucet is turned on."
+author= "-Louis Amour"
+print(f' "{quote}"')
+print(f'__{author}')
+
+#6
+person_name ="-Dorothy Parker "
+message ='''“If you want to know what God thinks of money, just look at the people he gave it to.” '''
+print(f'quote {person_name},{message}')
+
+#7
+person_name =" \t Rod Stewart \n"
+print(person_name)
+print(person_name.rstrip())
+print(person_name.rstrip())
+print(person_name.strip())
+      
+#8
+x=5
+y=10
+z=15
+sum=x+y+z
+
+#9
+a=5
+b=10
+a,b=b,a
+print(f'before swap:a={a},b={b}')
+print(f'after swap:a={a},b={b}')
+
+#10
+favorite_color="blue"    
+print(favorite_color)
+new_color="Black"
+print(new_color)
+
+#11
+pet_name="Buddy"      
+pet_name="Max"
+print(pet_name)
+
+#12
+sunshine=True
+print(sunshine)
+
+#13
+score=100
+print(score)
+score=200
+print(score)
+
+#14
+city="karachi"
+print(city)
+
+#15
+text="python_programming"
+print(text.title())
+
+#16
+text="you are great"
+print(text.lower())
+
+#17
+text="you look beautiful"
+print(text.upper())
+
+#18
+temperature=25
+print(f'the current temperature {temperature} degree')
+
+#19
+poem="""Love, like a mountain-wind upon an oak,
+Falling upon me, shakes me leaf and bough."""
+print(poem)
+
+
+
+
+
+
+
+
+
+
